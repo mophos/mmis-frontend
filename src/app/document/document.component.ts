50069a44f196c69fb3075c1fa6d6379f8dda0859
naphattharawat <naphattharawat@gmail.com>
2018-05-29 23:21:54 +0700

    update

--- src/app/document/document.component.ts
+++ src/app/document/document.component.ts
@@ -25,12 +25,12 @@ export class DocumentComponent implements OnInit {
   }
 
   downloadTemplate() {
-    const url = `${this.apiUrl}/pdf/template.xlsx`;
+    const url = `${this.apiUrl}/template/template.xlsx`;
     window.open(url, '_blank');
   }
 
   downloadTemplateBlank() {
-    const url = `${this.apiUrl}/pdf/template_blank.xlsx`;
+    const url = `${this.apiUrl}/template/template_blank.xlsx`;
     window.open(url, '_blank');
   }
 
@@ -51,6 +51,36 @@ export class DocumentComponent implements OnInit {
 
   downloadSQLBlank() {
     const url = `${this.apiUrl}/sql/mmis_blank.sql`;
+    window.open(url, '_blank');
+  }
+
+  downloadIssueHis() {
+    const url = `${this.apiUrl}/template/issueHis.xls`;
+    window.open(url, '_blank');
+  }
+
+  downloadIssueAdmin() {
+    const url = `${this.apiUrl}/template/issueAdmin.xls`;
+    window.open(url, '_blank');
+  }
+
+  downloadTeamViewer() {
+    const url = `${this.apiUrl}/program/teamviewerV13.zip`;
+    window.open(url, '_blank');
+  }
+
+  downloadCentOS7() {
+    const url = `https://drive.google.com/file/d/1pyX7LxGWhIZOjnPNqQRSbkzvus-HHMdX/view?usp=sharing`;
+    window.open(url, '_blank');
+  }
+
+  downloadDockerInstall() {
+    const url = `${this.apiUrl}/pdf/dockerInstall.pdf`;
+    window.open(url, '_blank');
+  }
+
+  downloadNavicat() {
+    const url = `https://www.navicat.com/en/download/navicat-for-mysql`;
     window.open(url, '_blank');
   }
 
